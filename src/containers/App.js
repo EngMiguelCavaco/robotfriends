140a82892bf55acfb11b976ce0fafa9b6786ac6d
Miguel Cavaco <md.cavaco@gmail.com>
2024-10-26 21:25:33 +0100

    perform audit and fix

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, useMemo} from "react";
+import React, {useState, useEffect} from "react";
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
 import Scroll from "../components/Scroll";
