4f44260213c0704cb62d072a95119a1716ef15e2
Miguel Cavaco <md.cavaco@gmail.com>
2024-10-26 20:47:17 +0100

    Final version: oldApp,js component with classes(not used) and App.js with functions(used).

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
-import React, {Fragment} from 'react';
+import React/*, {Fragment}*/ from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import "tachyons";
-import App from "./App"
+import App from "./containers/App"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>
-  <Fragment>
       <App />
-  </Fragment>  
-  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
